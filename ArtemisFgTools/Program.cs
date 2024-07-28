9048649396edd2ff51f960f18d1a59f7c374c2bb
Chenx221 <chenx221@yandex.com>
2024-07-28 09:48:31 +0800

    添加项目文件。

---
+++ ArtemisFgTools/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtemisFgTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
